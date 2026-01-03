c082adad7998f65bd04ca51070e21dffba4a7531
ElMaez <mai_ez@outlook.dk>
2026-01-03 17:11:34 +0100

    rettelser til forside tekst.

--- src/api/Forside.jsx
+++ src/api/Forside.jsx
@@ -1,11 +1,11 @@
 export const Forside = [
   { text: "Bruger du for mange timer på regnskabet?" },
-  { text: "Du behøver ikke stå med det hele selv!" },
+  { text: "Driller e-conomic i hverdagen?" },
   { text: "Tager driften fokus fra det, der er vigtigt?" },
-  { text: "Fylder regnskab og administration mere end de burde?" },
-  { text: "Du behøver ikke have styr på det hele!" },
-  { text: "Fylder administrationen for meget i hverdagen?" },
-  { text: "Der er hjælp at hente i hverdagen!" },
-  { text: "Bruger du tid på opgaver, andre kunne løse bedre?" },
+  { text: "Fylder regnskab og administration for meget?" },
+  { text: "Er du i tvivl om, hvad næste skridt er?" },
+  { text: "Vil du gerne gøre det rigtigt – uden stress?" },
+  { text: "Mangler du overblik i hverdagen?" },
+  { text: "Går tiden med opgaver, andre kunne løse bedre?" },
   { text: "Er der for mange løse ender i hverdagen?" },
 ];
